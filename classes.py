2459da39f7489a8fa292d7cb7682a3aee18e6ce5
LuizGOCunha <valuiz1@gmail.com>
2022-07-03 15:52:57 -0300

    Code Operational! Now we only need to refactor the random delivery and measure the performance for different methods

--- classes.py
+++ classes.py
@@ -2,6 +2,7 @@ from time import sleep
 from random import randint
 from uuid import uuid4
 import multiprocessing
+import csv
 
 # function to make the creation of paralel processes easier
 def create_paralel_process(func, list_of_args=[]):
@@ -28,7 +29,7 @@ class Kitchen:
         print(1)
         # We create the id for the order and then activate the Courier
         order_id = uuid4().hex
-        print(2)
+        print(order_id)
         # Create a paralel process to dispatch the courier
         dispatch_process = create_paralel_process(self.couriers.dispatch_order, list_of_args=[order_id,])
         dispatch_process.start()
@@ -42,7 +43,6 @@ class Kitchen:
         print(5)
         # This will put prepared food on the counter
         self.counter.put_food(prepared_food)
-        print(self.counter.counter_top)
         print(f'Order to cook {dish.name} with code {order_id} is Done!')
 
 
@@ -77,7 +77,7 @@ class Clients:
         self.kitchen = kitchen
 
     def initiate_ordering(self):
-        # Lets limit the amount with counter
+        # Lets limit the amount with a counter
         i = 0
         # Clients will be unrelenting ordering machines, to keep our system functioning
         while i != 1:
@@ -96,22 +96,51 @@ class Clients:
         self.kitchen.cook(order)
 
 class Counter:
-    # The counter will be the place where the food will be put after its done.
+    # The counter will be a csv file, the class object will be the gateway for the csv file
     # Here it will wait for a courier
-    def __init__(self):
-        self.counter_top = []
+
+    def return_complex_countercsv_list(self,clear_file=False) -> list:
+        '''This function just splits the csv file by line, then by comma, returning a 2D list'''
+        with open('counter.csv', 'r') as csv_file:
+            csv_string = csv_file.read()
+            csv_rows_list = csv_string.split('\n')
+            csv_complex_rows_list = []
+            for row in csv_rows_list:
+                list_row = row.split(',')
+                csv_complex_rows_list.append(list_row)
+            if clear_file:
+                # Dont panic, this is just so we can clear the file
+                with open('counter.csv', 'w') as _:
+                    pass
+        return csv_complex_rows_list
+
+    def write_data_to_countercsv(self,line_list):
+        '''Receives a list with the name and number of food order to be inserted into the csv file'''
+        line = line_list[0] + ',' + line_list[1] + '\n'
+        with open('counter.csv', 'a') as csv_file:
+            csv_file.write(line)
+
+    def remove_line_from_countercsv(self,id):
+        '''Receives an id to be remove, then pulls all data from csv, store it in a variable, 
+        then puts it back in excluding the line with the mentioned id. 
+        REMEMBER!!: The id must be a string, otherwise the function won't work'''
+        csv_list = self.return_complex_countercsv_list(clear_file=True)
+        for line_list in csv_list:
+            try:
+                if line_list[1] == id:
+                    continue
+                else: 
+                    self.write_data_to_countercsv(line_list)
+            except IndexError:
+                continue
 
     def put_food(self, food):
-        self.counter_top.append(food)
-        print(f'{food.name} with the code {food.id} is waiting to be delivered')
+        line_list = [f'{food.name}', f'{food.id}']
+        self.write_data_to_countercsv(line_list)
 
-    def remove_food(self, food_index):
-        self.counter_top.pop(food_index)
+    def remove_food(self, food_id):
+        self.remove_line_from_countercsv(food_id)
 
-    def __str__(self):
-        string = ''
-        for item in self.counter_top:
-            string += item.name + ', '
 
 
 class Courier:
@@ -121,6 +150,7 @@ class Courier:
         self.counter = counter
 
     def dispatch_random(self):
+        '''NEEDS TO BE REFACTORED'''
         # This type of dispatch make it so the courier grab the food that is ready randomly
         # The Courier always takes some time to get to the counter top
         sleep(randint(3,16))
@@ -133,9 +163,19 @@ class Courier:
 
     def dispatch_order(self, order_id):
         # The courier looks for the food with his order id, if he doesn't find it, he keeps looking
+        time_to_get_there = randint(3,15)
+        sleep(time_to_get_there)
+        print(f'Courier has arrived to take order with id {order_id}')
         while True:
-            for index, food in enumerate(self.counter.counter_top):
-                if food.id == order_id:
-                    self.counter.remove_food(index)
-                    print(f'{food.name} with the id {food.id} is delivered!')
-                    break
+            # This loop is the equivalent of the courier watching the countertop until his order arrives
+            counter_contents = self.counter.return_complex_countercsv_list()
+            for food in counter_contents:
+                # check the id of the food (second index for each line)
+                try:
+                    if food[1] == order_id:
+                        self.counter.remove_food(order_id)
+                        print(f'{food[0]} with the id {food[1]} is delivered!')
+                        break
+                # This is just so when it finds an empty string our code doesn't break, going to the next iteration instead
+                except IndexError:
+                    continue
